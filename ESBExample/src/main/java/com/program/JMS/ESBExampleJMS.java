6aaf4e861f9847ea44d0f711182bb2c7625977f6
sandychowdary.535 <sandychowdary.535@gmail.com>
2014-06-13 18:07:47 +0530

    committed ESBExample

---
+++ ESBExample/src/main/java/com/program/JMS/ESBExampleJMS.java
@@ -0,0 +1,5 @@
+package com.program.JMS;
+
+public class ESBExampleJMS {
+
+}
---
+++ ESBExample/src/main/java/com/program/action/ESBExampleAction.java
@@ -0,0 +1,39 @@
+package com.program.action;
+
+import org.jboss.soa.esb.actions.AbstractActionLifecycle;
+import org.jboss.soa.esb.client.ServiceInvoker;
+import org.jboss.soa.esb.couriers.FaultMessageException;
+import org.jboss.soa.esb.helpers.ConfigTree;
+import org.jboss.soa.esb.listeners.message.MessageDeliverException;
+import org.jboss.soa.esb.message.Message;
+import org.jboss.soa.esb.services.registry.RegistryException;
+
+public class ESBExampleAction extends AbstractActionLifecycle {
+	ConfigTree configTree;
+
+	public ESBExampleAction(ConfigTree configTree) {
+		this.configTree = configTree;
+	}
+
+	public Message process(Message message) throws MessageDeliverException,
+			FaultMessageException, RegistryException {
+
+		message.getProperties().setProperty("name", "sandy");
+		System.out
+				.println("$$$!!!!!!!!!!!!!!!!!!!!!$$$$$$$$ IN ESB Example Action class For serice 1 $$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		ServiceInvoker invoker = new ServiceInvoker("test", "testService2");
+		invoker.deliverAsync(message);
+		return message;
+	}
+
+	public Message secondService(Message message) {
+
+		System.out
+				.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$ IN ESB Example Action class For serice 2 $$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&& "
+				+ message.getProperties().getProperty("name")
+				+ " &&&&&&&&&&&&&&&&&&&&&&&&&&&");
+		return message;
+	}
+
+}
