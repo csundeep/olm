21b9c2bf5c235177b6515c8c135230d9848d8615
sandychowdary.535 <sandychowdary.535@gmail.com>
2014-06-30 17:31:58 +0530

    Added two new operations in WSDL forgot password,reset password

--- User_Manager/src/main/java/com/bookworms/usermanager/impl/UserManagerImpl.java
+++ User_Manager/src/main/java/com/bookworms/usermanager/impl/UserManagerImpl.java
@@ -7,18 +7,21 @@ import javax.jws.WebService;
 
 import com.bookworms.usermanager.AddUser;
 import com.bookworms.usermanager.AddUserResponse;
+import com.bookworms.usermanager.ForgotPasswordRequest;
+import com.bookworms.usermanager.ForgotPasswordResponse;
 import com.bookworms.usermanager.GetUserRequest;
+import com.bookworms.usermanager.ResetPasswordRequest;
+import com.bookworms.usermanager.ResetPasswordResponse;
 import com.bookworms.usermanager.SignInResponse;
 import com.bookworms.usermanager.SignInUser;
 import com.bookworms.usermanager.UserManagerPortType;
 
 @WebService(serviceName = "UserManagerImpl", targetNamespace = "http://bookWorms.com/usermanager", endpointInterface = "com.bookworms.usermanager.UserManagerPortType")
 public class UserManagerImpl implements UserManagerPortType {
-
-	@WebResult(name = "addUserResponse", targetNamespace = "http://bookWorms.com/usermanager", partName = "getUserResponse")
+	@WebResult(name = "resetPasswordResponse", targetNamespace = "http://bookWorms.com/usermanager", partName = "resetPasswordResponse")
 	@WebMethod
-	public AddUserResponse getUser(
-			@WebParam(partName = "getUserRequest", name = "getUserRequest", targetNamespace = "http://bookWorms.com/usermanager") GetUserRequest getUserRequest) {
+	public ResetPasswordResponse resetPassword(
+			@WebParam(partName = "resetPasswordRequest", name = "resetPasswordRequest", targetNamespace = "http://bookWorms.com/usermanager") ResetPasswordRequest resetPasswordRequest) {
 		return null;
 	}
 
@@ -29,10 +32,24 @@ public class UserManagerImpl implements UserManagerPortType {
 		return null;
 	}
 
+	@WebResult(name = "forgotPasswordResponse", targetNamespace = "http://bookWorms.com/usermanager", partName = "forgotPasswordResponse")
+	@WebMethod
+	public ForgotPasswordResponse forgotPassword(
+			@WebParam(partName = "forgotPasswordRequest", name = "forgotPasswordRequest", targetNamespace = "http://bookWorms.com/usermanager") ForgotPasswordRequest forgotPasswordRequest) {
+		return null;
+	}
+
 	@WebResult(name = "addUserResponse", targetNamespace = "http://bookWorms.com/usermanager", partName = "addUserResponse")
 	@WebMethod
 	public AddUserResponse addUser(
 			@WebParam(partName = "addUserRquest", name = "addUserRequest", targetNamespace = "http://bookWorms.com/usermanager") AddUser addUserRquest) {
+		return null;
+	}
+
+	@WebResult(name = "addUserResponse", targetNamespace = "http://bookWorms.com/usermanager", partName = "getUserResponse")
+	@WebMethod
+	public AddUserResponse getUser(
+			@WebParam(partName = "getUserRequest", name = "getUserRequest", targetNamespace = "http://bookWorms.com/usermanager") GetUserRequest getUserRequest) {
 		return null;
 	}
 
